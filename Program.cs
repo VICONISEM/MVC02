2722df7f1a30794a48a74b2a260d78b53fe857d9
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-09-07 23:00:51 +0300

    Action Result (First Controller)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using MVC02.Controllers;
+
 namespace MVC02
 {
     public class Program
@@ -5,53 +7,65 @@ namespace MVC02
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
             var app = builder.Build();
+           
 
-            app.UseRouting();
+            #region Old Routing 
+            //app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello From master");
-                });
-
-
-
-
-
-
-
-                endpoints.MapGet("/Home", async context =>
-                {
-                    await context.Response.WriteAsync("Hello From Home");
-                });
-
-
-                //endpoints.MapGet("/Products", async context =>
-                //{
-                //    await context.Response.WriteAsync("Hello From Products");
-                //});
-
-
-
-                endpoints.MapGet("/Products/{ID:int?}", async context =>
-                {
-                    await context.Response.WriteAsync($"Hello From Products id =>{context.Request.RouteValues["ID"]??"Null ID"}");
-                });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello From master");
+            //    });
 
 
 
 
 
 
-                endpoints.MapGet("/Books/{Auther:alpha}/{ID:int?}", async context =>
-                {
-                    await context.Response.WriteAsync($"Book with id=>{context.Request.RouteValues["ID"]??"Null ID"} , Aother => {context.Request.RouteValues["Auther"]}");
-                });
-            });
 
-            app.Run(async (HttpContext) => { await HttpContext.Response.WriteAsync("This Page not Exsite"); });
+            //    endpoints.MapGet("/Home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello From Home");
+            //    });
+
+
+            //    //endpoints.MapGet("/Products", async context =>
+            //    //{
+            //    //    await context.Response.WriteAsync("Hello From Products");
+            //    //});
+
+
+
+            //    endpoints.MapGet("/Products/{ID:int?}", async context =>
+            //    {
+            //        await context.Response.WriteAsync($"Hello From Products id =>{context.Request.RouteValues["ID"] ?? "Null ID"}");
+            //    });
+
+
+
+
+
+
+            //    endpoints.MapGet("/Books/{Auther:alpha}/{ID:int?}", async context =>
+            //    {
+            //        await context.Response.WriteAsync($"Book with id=>{context.Request.RouteValues["ID"] ?? "Null ID"} , Aother => {context.Request.RouteValues["Auther"]}");
+            //    });
+            //}); 
+            #endregion
+
+
+            app.MapControllerRoute(
+                name: "default"
+                , pattern: "/{Controller=Home}/{Action=Index}"
+                , defaults:new {Controller="Home", Action="Index" }
+
+                );
+
+           // app.Run(async (HttpContext) => { await HttpContext.Response.WriteAsync("This Page not Exsite"); });
             app.Run();
 
          
